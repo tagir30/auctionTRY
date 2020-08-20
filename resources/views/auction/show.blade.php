36f130c752f79b4689339ce07f720ad42200898d
tagir30 <kraev30@mail.ru>
2020-08-20 16:10:47 +0400

    добавил возможность комментировать

--- resources/views/auction/show.blade.php
+++ resources/views/auction/show.blade.php
@@ -3,24 +3,9 @@
 @section('content')
 
     <lot-show-component :lot="{{$lot}}" :user_id="@json(auth()->id())" :auth = "@json(auth()->check())"></lot-show-component>
-    {{--    <div class="container">--}}
-{{--        <div class="row no-gutters">--}}
-{{--            <div class="col-12 col-sm-6 col-md-5">--}}
-{{--                <img class="card-img-top"--}}
-{{--                     style="height: 300px; width: 300px; display: block;"--}}
-{{--                     src="{{asset('/storage/' . $lot->pathImage)}}"--}}
-{{--                     data-holder-rendered="true">--}}
-{{--            </div>--}}
+    <br>
+    <div class="container">
+        @comments(['model' => $lot])
+    </div>
 
-{{--            <div class="col-md-7">--}}
-{{--                <ul class="list-group">--}}
-{{--                    <li class="list-group-item">Название: {{$lot->name}}</li>--}}
-{{--                    <li class="list-group-item">Описание: {{$lot->description}}</li>--}}
-{{--                    <li class="list-group-item">Актуальная ставка: {{$lot->offer->bet_on_lot}}</li>--}}
-{{--                    <li class="list-group-item">Дата окончания торгов: {{$lot->timeLeft}}</li>--}}
-
-{{--                </ul>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
 @endsection
